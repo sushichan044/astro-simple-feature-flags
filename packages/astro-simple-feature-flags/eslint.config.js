24c3275e10795d16103d33cddb680d0ba5e73b6a
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-08-05 04:13:39 +0900

    chore: prohibit side effects

--- packages/astro-simple-feature-flags/eslint.config.js
+++ packages/astro-simple-feature-flags/eslint.config.js
@@ -1,1 +1,12 @@
-export { default } from "@virtual-live-lab/eslint-config/presets/ts";
+import ts from "@virtual-live-lab/eslint-config/presets/ts";
+import topLevel from "eslint-plugin-toplevel";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig(ts, {
+  plugins: {
+    toplevel: topLevel,
+  },
+  rules: {
+    "toplevel/no-toplevel-side-effect": "error",
+  },
+});
