99449bdb95279531ff6fb377e4b21210cc1094ed
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-08-06 21:23:46 +0900

    test: explicitly use fork pool

--- packages/e2e/vitest.config.ts
+++ packages/e2e/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    pool: "forks", // Must use forks since calling `process.chdir` in the test setup
     setupFiles: ["vitest-setup.ts"],
     unstubEnvs: true, // https://vitest.dev/guide/mocking.html#mock-import-meta-env
   },
