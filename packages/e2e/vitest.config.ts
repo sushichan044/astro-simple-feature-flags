38dfefbe7a86b28a3cf1927c996553cd00d32f7e
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-08-07 20:47:28 +0900

    set timeout

--- packages/e2e/vitest.config.ts
+++ packages/e2e/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     isolate: false,
     pool: "forks", // Must use forks since calling `process.chdir` in the test setup
     setupFiles: ["vitest-setup.ts"],
+    testTimeout: 10_000, // e2e tests run `astro build` in each case, so it may take longer
     unstubEnvs: true, // https://vitest.dev/guide/mocking.html#mock-import-meta-env
   },
 });
