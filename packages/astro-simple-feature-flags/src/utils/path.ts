6605d11d8d44b0caed259be07e223b0002c5a069
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-08-08 12:22:25 +0900

    chore: remove dead code

--- packages/astro-simple-feature-flags/src/utils/path.ts
+++
@@ -1,5 +0,0 @@
-export const stripLastExtension = (path: string): string => {
-  const lastDotIndex = path.lastIndexOf(".");
-  if (lastDotIndex === -1) return path; // No extension found
-  return path.slice(0, lastDotIndex);
-};
